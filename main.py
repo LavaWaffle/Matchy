86d0a8c58aec05d5e9dd6db3f83ed0e3fb7d0843
KinuTheDragon <91227747+KinuTheDragon@users.noreply.github.com>
2023-01-27 16:00:27 -0500

    grids & multi-team graphing

--- main.py
+++ main.py
@@ -19,6 +19,18 @@ OUR_TEAM = 293
 IMAGE_DIR = "images"
 EVENT_KEYS = ["njski", "njrob"]
 
+TEAM_COLORS = [
+    "#000000",
+    "#ff0000",
+    "#0000ff",
+    "#00ffff",
+    "#ff00ff",
+    "#cc00ff",
+    "#ff8800",
+    "#888888",
+    "#44cc44"
+]
+
 if os.path.exists(IMAGE_DIR):
     for file in os.listdir(IMAGE_DIR):
         os.remove(os.path.join(IMAGE_DIR, file))
@@ -87,7 +99,8 @@ EPA: {team_data.norm_epa if team_data.norm_epa is not None else 'unknown'} (aver
 @bot.command()
 @lightbulb.option("teams2", "Comma-separated list of team numbers on alliance #2", str)
 @lightbulb.option("teams1", "Comma-separated list of team numbers on alliance #1", str)
-@lightbulb.command("predict", "Compares two alliances and predicts the outcome")
+@lightbulb.command("predict", "Compares two alliances and predicts the outcome",
+                   auto_defer = True)
 @lightbulb.implements(lightbulb.SlashCommand, lightbulb.PrefixCommand)
 async def compare(ctx: lightbulb.Context) -> None:
     try:
@@ -191,43 +204,57 @@ async def yearepa(ctx: lightbulb.Context) -> None:
     plt.xlabel("Percentile")
     plt.ylabel("EPA")
     plt.title(f"Year {ctx.options.year}'s EPA")
+    plt.grid(which = "both", axis = "both", color = "#cccccc")
     await send_plot(ctx, f"EPA data for {ctx.options.year}")
 
 
 @bot.command()
-@lightbulb.option("team", "The team number to check", int)
-@lightbulb.command("teamepa", "Graphs a team's EPA over the years",
+@lightbulb.option("teams", "The team numbers to check, separated by commas", str)
+@lightbulb.command("teamepa", "Graphs teams' EPA over the years",
                    auto_defer = True)
 @lightbulb.implements(lightbulb.SlashCommand, lightbulb.PrefixCommand)
 async def teamepa(ctx: lightbulb.Context):
     if not os.path.exists(IMAGE_DIR):
         os.mkdir(IMAGE_DIR)
-    team = Team(ctx.options.team)
-    if team.data is None:
-        await error(ctx, f"There is no team numbered {team.team_number}!")
+    try:
+        teams = [Team(int(x.strip())) for x in ctx.options.teams.split(",")]
+    except ValueError:
+        await error(ctx, "Invalid team number!")
         return
-    years, epas = [], []
-    for year in range(team.rookie_year, time.localtime().tm_year + 1):
-        try:
-            year_data = sb.get_team_year(team.team_number, year)
-            epa = year_data["norm_epa_end"]
-            if epa is not None:
-                years.append(year)
-                epas.append(epa)
-        except UserWarning:
-            pass
-    plt.plot(years, epas, "o-", color = "#000000")
+    if len(teams) > len(TEAM_COLORS):
+        await error(ctx, f"Please use at most {len(TEAM_COLORS)} teams!")
+        return
+    for team in teams:
+        if team.data is None:
+            await error(ctx, f"There is no team numbered {team.team_number}!")
+            return
+    year_set = set()
+    for i, team in enumerate(teams):
+        years, epas = [], []
+        for year in range(team.rookie_year, time.localtime().tm_year + 1):
+            try:
+                year_data = sb.get_team_year(team.team_number, year)
+                epa = year_data["norm_epa_end"]
+                if epa is not None:
+                    years.append(year)
+                    epas.append(epa)
+            except UserWarning:
+                pass
+        line, = plt.plot(years, epas, "o-", color = TEAM_COLORS[i])
+        line.set_label(f"Team {team.team_number}")
+        year_set = year_set.union(set(years))
     xlim = plt.gca().get_xlim()
     line, = plt.plot([0, 2 * years[-1]], [1500, 1500], "-", color = "#00ff00")
     line.set_label("Baseline")
     plt.xlim(xlim)
     plt.ylabel("EPA")
-    plt.title(f"Team {team.team_number}'s EPA")
+    plt.title(f"Team EPAs")
     plt.legend()
-    plt.xticks(years)
+    plt.xticks(sorted(list(year_set)))
+    plt.grid(which = "both", axis = "both", color = "#cccccc")
     ax = plt.gca()
     ax.set_xticklabels(ax.get_xticks(), rotation = 90)
-    await send_plot(ctx, f"EPA data for team {ctx.options.team}")
+    await send_plot(ctx, f"EPA data for teams {', '.join(str(i.team_number) for i in teams)}")
 
 
 @bot.command()
@@ -289,6 +316,6 @@ async def everyone(ctx: lightbulb.Context):
 
 
 try: bot.run()
-except KeyError:
+except (hikari.errors.HTTPResponseError, KeyError):
     if IS_REPLIT: os.system("kill 9")
     else: print("Please restart me")
