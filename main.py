1a087064d7272451bad5543c010280b8edc5027d
KinuTheDragon <91227747+KinuTheDragon@users.noreply.github.com>
2023-01-28 12:55:17 -0500

    Added comments regarding command loading

--- main.py
+++ main.py
@@ -29,31 +29,42 @@ else:
     )
 
 
+# To make a command (with the module, decorators, and option names):
 def make_command(cmd, decorators, options):
+    # Create a wrapper
     async def wrapper(ctx: lightbulb.Context):
+        # Unpack options and pass to run
         await cmd.run(ctx, *[getattr(ctx.options, option) for option in options])
+    # Apply decorators
     for decorator in decorators:
         wrapper = decorator(wrapper)
 
 
-for pcmd in dir(commands):
-    cmd = getattr(commands, pcmd)
+# For each possible command:
+for possible_cmd in dir(commands):
+    cmd = getattr(commands, possible_cmd)
+    # Ensure that it has a run method
     if not hasattr(cmd, "run"): continue
+    # Deal with lightbulb.implements
     impl = lightbulb.implements(lightbulb.SlashCommand, lightbulb.PrefixCommand)
     if getattr(cmd, "SLASH_ONLY", False):
         impl = lightbulb.implements(lightbulb.SlashCommand)
+    # Initialize decorators list
     decorators = [
         impl,
         lightbulb.command(cmd.NAME, cmd.DESCRIPTION, auto_defer = True)
     ]
+    # Get arguments (black magic with inspect go brrr) - trim ctx from start
     args = list(inspect.signature(cmd.run).parameters.values())[1:]
     for arg in args:
+        # Create lightbulb.option decorator
         these_args = [arg.name, arg.annotation[0], arg.annotation[1]]
         these_kwargs = {}
         if arg.default != inspect._empty:
             these_kwargs["required"] = False
             these_kwargs["default"] = arg.default
         decorators.append(lightbulb.option(*these_args, **these_kwargs))
+    # The grand finale
     decorators.append(bot.command())
     make_command(cmd, decorators, [arg.name for arg in args])
 
